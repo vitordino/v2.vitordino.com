b4762193af33fca654a0b82112495fa391e10e29
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-30 03:25:15 -0200

    :lipstick: theme: parametric colors

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,21 @@
+import {mix} from 'polished'
+
 const fontStack = "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"
+
+const base = '#000'
+const white = '#FFF'
 
 const theme = {
 	colors: {
-		black: 'black',
-		white: 'white',
+		white: white,
+		base: base,
+		base88: mix(0.88, base, white),
+		base66: mix(0.66, base, white),
+		base44: mix(0.44, base, white),
+		base22: mix(0.22, base, white),
+		base11: mix(0.11, base, white),
+		base06: mix(0.06, base, white),
+		base03: mix(0.03, base, white),
 	},
 	fonts: {
 		default: `FaktPro, ${fontStack}`
