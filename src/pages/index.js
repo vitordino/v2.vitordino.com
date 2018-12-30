ed231fa95bf7846ab9066832c70f79a6bb57d094
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-30 03:27:20 -0200

    :sparkles: first index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,65 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
+import above from '../utils/above'
+import {flattenEdges} from '../utils/data'
 import Layout from '../components/Layout'
+import {Heading} from '../components/Text'
+import List from '../components/List'
 
-const IndexPage = ({data: {generalObject}}) => {
-	const general = generalObject.frontmatter
-	return (
-		<Layout>
-			hey home
-		</Layout>
-	)
-}
+const Wrapper = styled.div`
+	padding: 1.5rem 1rem;
+	${above('sm')`padding: 2.5rem 1.5rem;`}
+	${above('md')`padding: 3.125rem 2rem;`}
+	${above('lg')`padding: 4.25rem 3.5rem;`}
+	${above('xg')`padding: 3.75rem 3.5rem;`}
+`
+
+const IndexPage = ({data: {allExperience, allProjects}}) => (
+	<Layout>
+		<Wrapper>
+			<Heading weight={300} color={p => p.theme.colors.base44}>
+				move fast, learn everyday
+			</Heading>
+			<List title='Experiences' items={flattenEdges(allExperience)}/>
+			<List title='Projects' items={flattenEdges(allProjects)}/>
+		</Wrapper>
+	</Layout>
+)
 
 export default IndexPage
 
 export const pageQuery = graphql`
 	query IndexQuery {
-		generalObject: markdownRemark(frontmatter: { template: { eq: "general" } }) {
-			frontmatter {
-				title
+		allExperience: allMarkdownRemark(
+			filter: {frontmatter: { template: { eq: "experience" } }}
+		) {
+			edges {
+				node {
+					html
+					frontmatter {
+						title
+						date
+						description
+						to
+					}
+				}
+			}
+		}
+		allProjects: allMarkdownRemark(
+			filter: {frontmatter: { template: { eq: "project" } }}
+		) {
+			edges {
+				node {
+					html
+					frontmatter {
+						title
+						description
+						date
+						to
+						color
+					}
+				}
 			}
 		}
 	}
